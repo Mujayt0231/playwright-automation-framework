09b577d95167057150cfe60dacca9144a1877638
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-08-26 18:01:39 -0400

    refactor test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import * as os from "os";
 
 /**
  * Read environment variables from file.
@@ -11,6 +12,21 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 5000,
+
+    toHaveScreenshot: {
+      // An acceptable amount of pixels that could be different, unset by default.
+      maxDiffPixels: 10,
+    },
+
+    toMatchSnapshot: {
+      // An acceptable ratio of pixels that are different to the
+      // total amount of pixels, between 0 and 1.
+      maxDiffPixelRatio: 0.1,
+    },
+  },
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -21,13 +37,30 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["allure-playwright"], ["html"]],
+  reporter: [
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        suiteTitle: false,
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
+    ],
+    ["html"],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://www.aapc.com/",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    screenshot: "only-on-failure",
+    //video: "retain-on-failure",
     trace: "on-first-retry",
   },
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,42 +3,42 @@ import { testData } from "../src/data/testdata";
 import { LoginPage } from "../src/pages/LoginPage";
 import { HomePage } from "../src/pages/HomePage";
 
-test.describe.configure({ mode: "parallel" });
+test.describe("Login Suite", () => {
+  let loginPage: LoginPage;
+  let homePage: HomePage;
 
-let loginPage: LoginPage;
-let homePage: HomePage;
-
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
-  homePage = new HomePage(page);
-  await homePage.navigate();
-  await homePage.clickSignIn();
-});
-
-test("invalid credential for user login", async ({}) => {
-  await loginPage.login("test-automation@test.com", "Testing123.");
-  await loginPage.verifyInvalidLoginError();
-});
-
-test("valid credential for user login", async ({}) => {
-  await loginPage.login(testData.username, testData.password);
-  await loginPage.verifyDashboardVisible();
-});
-
-test("forget password page navigation", async ({ page }) => {
-  await page.getByRole("link", { name: "Forgot password?" }).click();
-  await expect(
-    page.getByRole("link", { name: "Go back to sign in" })
-  ).toBeVisible({ timeout: 5000 });
-});
-
-test("'already have an account' page navigation", async ({ page }) => {
-  await page.getByLabel("CREATE AN ACCOUNT").click();
-  await expect(page.getByText("Already have an account")).toBeVisible({
-    timeout: 5000,
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
+    await homePage.navigate();
+    await homePage.clickSignIn();
   });
-});
 
-test.afterEach(async ({ page }) => {
-  await page.close();
+  test("invalid credential for user login", async () => {
+    await loginPage.login("test-automation@test.com", "Testing123.");
+    await loginPage.verifyInvalidLoginError();
+  });
+
+  test("valid credential for user login", async () => {
+    await loginPage.login(testData.username, testData.password);
+    await loginPage.verifyDashboardVisible();
+  });
+
+  test("forget password page navigation", async ({ page }) => {
+    await page.getByRole("link", { name: "Forgot password?" }).click();
+    await expect(
+      page.getByRole("link", { name: "Go back to sign in" })
+    ).toBeVisible({ timeout: 5000 });
+  });
+
+  test("'already have an account' page navigation", async ({ page }) => {
+    await page.getByLabel("CREATE AN ACCOUNT").click();
+    await expect(page.getByText("Already have an account")).toBeVisible({
+      timeout: 5000,
+    });
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
 });
